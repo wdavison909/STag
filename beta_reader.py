15f98f85b274520b1e158185ab08d3ee2343cc17
wdavison909 <77482543+wdavison909@users.noreply.github.com>
2022-06-14 17:52:44 +0900

    Updated version of STag 2.0
    Removal of tags, addition of new tags, new model

--- beta_reader.py
+++ beta_reader.py
@@ -13,19 +13,18 @@ def beta_reader(direc):
     Function to read in beta values for each tag
     """
     path = direc
-
+    
     ii_ha_beta = np.loadtxt('%s/Beta Values/ii_halpha_beta_final3.txt' % path)
     Si_beta = np.loadtxt('%s/Beta Values/ia_si_beta_final5.txt' % path)
-    He_emi_beta = np.loadtxt('%s/Beta Values/he_emi_beta_final4.txt' % path)
-    He_cyg_beta = np.loadtxt('%s/Beta Values/he_cyg_beta_final4.txt' % path)
     He_abs_beta = np.loadtxt('%s/Beta Values/he_abs_beta_final4.txt' % path)
     Ca_beta = np.loadtxt('%s/Beta Values/cal_beta_final4.txt' % path)
-    he_6678_beta = np.loadtxt('%s/Beta Values/he_6678_beta_final2.txt' % path)
     fe_4924_beta = np.loadtxt('%s/Beta Values/fe_4924_beta_final2.txt' % path)
     s_beta = np.loadtxt('%s/Beta Values/s_beta_final4.txt' % path)
+    he_6450_beta = np.loadtxt('%s/Beta Values/he_6450_beta_final3.txt' % path)
     fe_5018_beta = np.loadtxt('%s/Beta Values/fe_5018_beta_final2.txt' % path)
     fe_5170_beta = np.loadtxt('%s/Beta Values/fe_5170_beta_final2.txt' % path)
     ii_hg_beta = np.loadtxt('%s/Beta Values/ii_hg_beta_final2.txt' % path)
     si_4000_beta = np.loadtxt('%s/Beta Values/si_4000_beta_final.txt' % path)
+    hbeta_beta = np.loadtxt('%s/Beta Values/hbeta_beta_final.txt' % path)
 
-    return ii_ha_beta,Si_beta,He_emi_beta,He_cyg_beta,He_abs_beta,Ca_beta,he_6678_beta,fe_4924_beta,s_beta,fe_5018_beta,fe_5170_beta,ii_hg_beta,si_4000_beta
+    return ii_ha_beta,Si_beta,He_abs_beta,Ca_beta,fe_4924_beta,s_beta,he_6450_beta,fe_5018_beta,fe_5170_beta,ii_hg_beta,si_4000_beta,hbeta_beta
